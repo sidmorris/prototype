5e78ea0d611fecb01f187abc9c22f727a04c2417
sidmorris <sidmorris@mail.ru>
2018-04-08 22:59:14 +0300

    metal init

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -1,44 +1,15 @@
 module.exports = {
   SASS: [
     // 'source/style',
-    'node_modules/foundation-sites/scss',
-    'node_modules/motion-ui/src'
+    // 'node_modules/foundation-sites/scss',
+		'node_modules/bootstrap/scss',
+		// 'node_modules/uikit/src/scss'
+    // 'node_modules/motion-ui/src'
   ],
   JS_FOUNDATION: [
-    'node_modules/jquery/dist/jquery.min.js',
-    'node_modules/foundation-sites/dist/js/plugins/foundation.core.js',//impotant
-    'node_modules/foundation-sites/dist/js/plugins/foundation.util.mediaQuery.js',//impotant
-    // 'node_modules/foundation-sites/dist/js/plugins/foundation.abide.js',
-    // 'node_modules/foundation-sites/dist/js/plugins/foundation.accordion.js',
-    // 'node_modules/foundation-sites/dist/js/plugins/foundation.accordionMenu.js',
-    // 'node_modules/foundation-sites/dist/js/plugins/foundation.drilldown.js',
-    // 'node_modules/foundation-sites/dist/js/plugins/foundation.dropdown.js',
-    // 'node_modules/foundation-sites/dist/js/plugins/foundation.dropdownMenu.js',
-    // 'node_modules/foundation-sites/dist/js/plugins/foundation.equalizer.js',
-    // 'node_modules/foundation-sites/dist/js/plugins/foundation.interchange.js',
-    // 'node_modules/foundation-sites/dist/js/plugins/foundation.magellan.js',
-    // 'node_modules/foundation-sites/dist/js/plugins/foundation.offcanvas.js',
-    // 'node_modules/foundation-sites/dist/js/plugins/foundation.orbit.js',
-    // 'node_modules/foundation-sites/dist/js/plugins/foundation.responsiveAccordionTabs.js',
-    // 'node_modules/foundation-sites/dist/js/plugins/foundation.responsiveMenu.js',
-    // 'node_modules/foundation-sites/dist/js/plugins/foundation.responsiveToggle.js',
-    // 'node_modules/foundation-sites/dist/js/plugins/foundation.reveal.js',
-    // 'node_modules/foundation-sites/dist/js/plugins/foundation.slider.js',
-    // 'node_modules/foundation-sites/dist/js/plugins/foundation.smoothScroll.js',
-    // 'node_modules/foundation-sites/dist/js/plugins/foundation.sticky.js',
-    // 'node_modules/foundation-sites/dist/js/plugins/foundation.tabs.js',
-    // 'node_modules/foundation-sites/dist/js/plugins/foundation.toggler.js',
-    // 'node_modules/foundation-sites/dist/js/plugins/foundation.tooltip.js',
-    // 'node_modules/foundation-sites/dist/js/plugins/foundation.util.box.js',
-    // 'node_modules/foundation-sites/dist/js/plugins/foundation.util.imageLoader.js',
-    // 'node_modules/foundation-sites/dist/js/plugins/foundation.util.keyboard.js',
-    // 'node_modules/foundation-sites/dist/js/plugins/foundation.util.motion.js',
-    // 'node_modules/foundation-sites/dist/js/plugins/foundation.util.nest.js',
-    // 'node_modules/foundation-sites/dist/js/plugins/foundation.util.timer.js',
-    // 'node_modules/foundation-sites/dist/js/plugins/foundation.util.timerAndImageLoader.js',
-    // 'node_modules/foundation-sites/dist/js/plugins/foundation.util.touch.js',
-    // 'node_modules/foundation-sites/dist/js/plugins/foundation.util.triggers.js',
-    // 'node_modules/foundation-sites/dist/js/plugins/foundation.zf.responsiveAccordionTabs.js'
+		'node_modules/jquery/dist/jquery.js',
+		// 'node_modules/uikit/dist/js/uikit.js',
+		// 'node_modules/uikit/dist/js/uikit-icons.js'
   ],
   CSS_FOUNDATION: [
     
--- gulp/tasks/foundation.js
+++ gulp/tasks/foundation.js
@@ -1,15 +1,18 @@
+let uglify = require('gulp-uglify-es').default;
+
+
 module.exports = function(){
-  $.gulp.task('css:foundation', function () {
-  return $.gulp.src($.PATH.CSS_FOUNDATION)
-    .pipe($.gp.concatCss('foundation.css'))
-    .pipe($.gp.csso())
-    .pipe($.gulp.dest($.PATH.ROOT + '/assets/css'))
-});
+//   $.gulp.task('css:foundation', function () {
+//   return $.gulp.src($.PATH.CSS_FOUNDATION)
+//     .pipe($.gp.concatCss('foundation.css'))
+//     .pipe($.gp.csso())
+//     .pipe($.gulp.dest($.PATH.ROOT + '/assets/css'))
+// });
 
 $.gulp.task('js:foundation', function () {
   return $.gulp.src($.PATH.JS_FOUNDATION)
     .pipe($.gp.concat('foundation.js'))
-    .pipe($.gp.uglify())
+    .pipe(uglify())
     .pipe($.gulp.dest($.PATH.ROOT + '/assets/js'))
 });
 };
